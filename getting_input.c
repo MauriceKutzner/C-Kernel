5c0fdb6d7850f636fd96eae0381c903bfe8a779d
MauriceKutzner <mauricekutzner9@gmail.com>
2025-04-12 10:49:08 +0200

    working on backspace functionality

--- getting_input.c
+++ getting_input.c
@@ -2,13 +2,19 @@
 #include <stdio.h>
 int main()
 {
+    char buffer[1000];
     int i= 0;
-    while(i<20){
+    printf("type # to end program\n\n");
+    while(1){
     char character = getch();
+    //printf("%d", character);
+    if (character == 35){break;}    //# as not so much used escape sequence
 
     if(character == 13){printf("\n");}
-    if(character == 127){printf(" ");}
+    if(character == 8){for(int n=0;n<i-1;n++){printf("%c", buffer[n]);}}
     else{printf("%c", character);}
+    buffer[i] = character;
+
     i++;
     }
 return 0;
